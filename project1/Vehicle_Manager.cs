6bad021e83e46cb9bfdba55796ff7524cd1255e6
Jason Dexter <jasondexter04@gmail.com>
2025-12-06 16:32:26 -0600

    Added Vehicle_Manager.

---
+++ project1/Vehicle_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project1;
+
+namespace project1
+{
+	public class Vehicle_Manger
+	{
+		public List<Car> vehicle_list;
+
+		public Vehicle_Manger()
+		{
+			this.vehicle_list = new List<Car>();
+		}
+
+		public void add_car(Car vehicle)
+		{
+			this.vehicle_list.Add(vehicle);
+		}
+
+		public void remove_car(int index)
+		{
+			this.vehicle_list.RemoveAt(index);
+		}
+
+		public void set_pwd(string pwd)
+		{
+			this.vehicle_list[index].Router_Menu(false, pwd);
+		}
+	}
+}
